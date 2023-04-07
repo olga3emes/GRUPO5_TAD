15bb4f304f4c6e7dc95d075f924ec0e6e5e68e29
Alejandro Román Caballero <32822670+Alejandro314@users.noreply.github.com>
2023-04-07 14:35:40 +0200

    Datos de prueba de productos
    Aqui añado los datos de prueba de producto, recordar hacer,
    php artisan migrate:fresh --seed

---
+++ CarUPO/database/factories/AccesorioFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Accesorio;
+use App\Models\Producto;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AccesorioFactory extends Factory
+{
+    protected $model = Accesorio::class;
+
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->word(),
+            'fk_producto_id' => Producto::factory()->create()->id,
+        ];
+    }
+}
